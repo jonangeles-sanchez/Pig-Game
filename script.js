ca509e93009a6d665da9c9d65b32e079479e4455
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2021-12-26 16:07:04 -0500

    Added hidden class, variables, functions, and dice image change.
    Created const variables to be assigned with element classes. These elements are then used in functions to change their values. The variables: scores, currentScore, and activePlayer were created. An event listener was added to the btnRoll that would perform a function to randomize a number on the die and display it using an image source. Then a conditional statement was added if the dice rolled a 1 or not.

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 'use strict';
+
+//Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1'); //Somewhat faster than querySelector
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll 1-6
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice); //For Testing
+
+  // 2. Display the dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1: if true, switch to next player.
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    //Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0; //if active player is 0, then switch to 1, else if player is already 1, then switch to 0.
+    currentScore = 0;
+  }
+});
