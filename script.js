f080381761dedd02daa10c3f81063dcf5bf2a5c4
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2021-12-28 13:21:33 -0500

    Disabled roll and hold button when a player has won
    Disabled these functions by using a boolean. If the users are still playing, the boolean will allow a condition to use the functions that allow the dice to roll and the player to hold their score. If it's false, then these functions will not be accessible when roll or hold buttons are pressed.

--- script.js
+++ script.js
@@ -22,57 +22,62 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
-  // 1. Generating a random dice roll 1-6
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice); //For Testing
+  if (playing) {
+    // 1. Generating a random dice roll 1-6
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice); //For Testing
 
-  // 2. Display the dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+    // 2. Display the dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  // 3. Check for rolled 1: if true, switch to next player.
-  if (dice !== 1) {
-    // Add dice to current score
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    //Switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0; //if active player is 0, then switch to 1, else if player is already 1, then switch to 0.
-    currentScore = 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    // 3. Check for rolled 1: if true, switch to next player.
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      //Switch to next player
+      document.getElementById(`current--${activePlayer}`).textContent = 0;
+      activePlayer = activePlayer === 0 ? 1 : 0; //if active player is 0, then switch to 1, else if player is already 1, then switch to 0.
+      currentScore = 0;
+      player0El.classList.toggle('player--active');
+      player1El.classList.toggle('player--active');
+    }
   }
 });
 
 //Hold button functionality
 btnHold.addEventListener('click', function () {
-  if (activePlayer == 0) {
-    scores[0] += currentScore;
-    score0El.textContent = scores[0];
-  } else {
-    scores[1] += currentScore;
-    score1El.textContent = scores[1];
-  }
-  if (score0El.textContent >= 100) {
-    player1El.classList.toggle('player--active');
-    player1El.classList.toggle('player--winner');
-    btnRoll.removeEventListener('click');
-  } else if (score1El.textContent >= 100) {
-    player0El.classList.toggle('player--active');
-    player0El.classList.toggle('player--winner');
-    btnRoll.removeEventListener('click');
-  } else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0; //if active player is 0, then switch to 1, else if player is already 1, then switch to 0.
-    currentScore = 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+  if (playing) {
+    if (activePlayer == 0) {
+      scores[0] += currentScore;
+      score0El.textContent = scores[0];
+    } else {
+      scores[1] += currentScore;
+      score1El.textContent = scores[1];
+    }
+    if (score0El.textContent >= 10) {
+      player1El.classList.toggle('player--active');
+      player1El.classList.toggle('player--winner');
+      playing = false;
+    } else if (score1El.textContent >= 10) {
+      player0El.classList.toggle('player--active');
+      player0El.classList.toggle('player--winner');
+      playing = false;
+    } else {
+      document.getElementById(`current--${activePlayer}`).textContent = 0;
+      activePlayer = activePlayer === 0 ? 1 : 0; //if active player is 0, then switch to 1, else if player is already 1, then switch to 0.
+      currentScore = 0;
+      player0El.classList.toggle('player--active');
+      player1El.classList.toggle('player--active');
+    }
   }
 });
 
-//Hold button functionality.
+//New Game functionality.
